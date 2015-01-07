9277f56f518815bd810abdf17a375aba1c1348f4
unknown <james.zhou@CA04316D.global.local>
2015-01-07 15:40:47 -0500

    move to using angularjs

--- js/controller.js
+++ js/controller.js
@@ -1,12 +1,147 @@
-$(document).on("pagecreate","#pageone",function(){
-  $("#getProjects").on("tap",function(){
-    $.ajax({
-        url: "http://gtech.bitnamiapp.com/ProjectTools/app/project"
-    }).then(function(data) {
-        for (var i in data) {
-            $('#result').append("<br>" + data[i].name);
-            data[i]
-        }
-    });
-  });
-});
+//$(document).on("pagecreate","#pageone",function(){
+//    var projects = [];
+//    $.ajax({
+//        url: "http://gtech.bitnamiapp.com/ProjectTools/app/project"
+//    }).then(function(data) {
+//        for (var i in data) {
+//            $('#projectList > tbody:last').append("<tr><td>" + data[i].name + "</td></tr>");
+//            projects = data;
+//        }
+//    });
+//    $("#projectList").delegate('tr',"tap",function(){
+//        alert(projects[$(this).index()].name);
+//    });
+//});
+
+var buildToolApp = angular.module('buildTool', ['ngRoute', 'buildTool.services', 'projectTool.datTimeFilter']);
+
+buildToolApp.config(['$routeProvider',
+    function($routeProvider){
+		$routeProvider
+		.when('/', {
+			templateUrl: 'view/dashboard.html',
+			controller: 'DashboardCtrl'
+		})
+		.when('/project/:name', {
+			templateUrl: 'view/project.html',
+			controller: 'ProjectCtrl'
+		})
+		.when('/project/create/new', {
+			templateUrl: 'view/projectConfig.html',
+			controller: 'ProjectConfigCtrl'
+		})
+		.when('/project/:name/config', {
+			templateUrl: 'buildTool/projectConfig.html',
+			controller: 'ProjectConfigCtrl'
+		})
+		.when('/repository', {
+			templateUrl: 'buildTool/repository.html',
+			controller: 'RepositoryListCtrl'
+		})
+		.when('/repository/edit/:id', {
+			templateUrl: 'buildTool/repositoryEdit.html',
+			controller: 'RepositoryEditCtrl'
+		})
+		.when('/repository/create', {
+			templateUrl: 'buildTool/repositoryEdit.html',
+			controller: 'RepositoryEditCtrl'
+		})
+		.when('/project/:name/build/:buildId', {
+			templateUrl: 'buildTool/build.html',
+			controller: 'BuildCtrl'
+		})
+		;
+	}
+]);
+
+buildToolApp.controller('DashboardCtrl', [
+	'$scope', 'Project',
+	function($scope,  Project){
+
+//		authenticate();
+		$scope.openAlert = function() {
+			$scope.launch("notify", "My Title", "<div>My Content <span style='color:red;'>hellow</span></div>");
+		};
+		confirmed = function(result) {
+			alert(result);
+		};
+
+		$scope.projects = Project.query();
+	}
+]);
+
+buildToolApp.controller('ProjectCtrl', [
+	'$scope', '$http', '$routeParams', 'Project', 'Build',
+	function($scope, $http, $routeParams, Project, Build){
+
+		$scope.project = Project.get({name: $routeParams.name}, function(project){
+			$scope.builds = Build.query({projectId: project.id},
+					function(builds) {
+						checkBuilds(builds);
+			});
+		});
+
+		$scope.build = function() {
+			$http.get("/ProjectTools/app/project/build/" + $scope.project.name).success(
+				function(data){
+					if (data.message == undefined) {
+						$scope.builds = Build.query({projectId: $scope.project.id},
+							function(builds) {
+						});
+					} else {
+						$scope.launch("notify", "Build notification", data.message);
+					}
+				}
+			);
+		};
+
+		var checkBuildProgress = function() {
+			$scope.builds = Build.query({projectId: $scope.project.id},
+				function(builds) {
+					checkBuilds(builds);
+			});
+		};
+
+//		var abortTimer = function() {
+//			clearTimeout(tid);
+//		};
+
+		var checkBuilds = function(builds) {
+			builds.forEach(function(build){
+				if (build.buildStatus != 4 && build.buildStatus != 5) {
+//					tid = setTimeout(checkBuildProgress, 2000);
+					return;
+				}
+			});
+		};
+
+		$scope.refresh = function() {
+			checkBuildProgress();
+		};
+
+		$scope.startSchedulerRun = function() {
+			$http.get("/ProjectTools/app/project/schedule/1/" + $scope.project.name).success(
+				function(data){
+					if (data.message == undefined) {
+						$scope.project.schedulerEnabled = 1;
+					} else {
+						$scope.launch("notify", "Build notification", data.message);
+					}
+				}
+			);
+		};
+
+		$scope.stopSchedulerRun = function() {
+			$http.get("/ProjectTools/app/project/schedule/0/" + $scope.project.name).success(
+				function(data){
+					if (data.message == undefined) {
+						$scope.project.schedulerEnabled = 0;
+					} else {
+						$scope.launch("notify", "Build notification", data.message);
+					}
+				}
+			);
+		};
+
+	}
+]);
---
+++ js/filter/dateTimeFilter.js
@@ -0,0 +1,84 @@
+var filterService = angular.module('projectTool.datTimeFilter', []);
+
+filterService.filter('timeElapsed', function() {
+	var timeElapsedFilter = function(input) {
+		return (new Date()).getDiff(input);
+	};
+	return timeElapsedFilter;
+});
+
+filterService.filter('duration', function() {
+	var curationFilter = function(input) {
+		return getDuration(input);
+	};
+	return curationFilter;
+});
+
+
+filterService.filter('dateTime', function() {
+	var dateTimeFilter = function(input) {
+		if (input == undefined)
+			return "";
+		return (new Date(input)).customFormat( "#YYYY#-#MM#-#DD# #hh#:#mm#:#ss#" );
+	};
+	return dateTimeFilter;
+});
+
+Date.prototype.customFormat = function(formatString){
+    var YYYY,YY,MMMM,MMM,MM,M,DDDD,DDD,DD,D,hhh,hh,h,mm,m,ss,s,ampm,AMPM,dMod,th;
+    var dateObject = this;
+    YY = ((YYYY=dateObject.getFullYear())+"").slice(-2);
+    MM = (M=dateObject.getMonth()+1)<10?('0'+M):M;
+    MMM = (MMMM=["January","February","March","April","May","June","July","August","September","October","November","December"][M-1]).substring(0,3);
+    DD = (D=dateObject.getDate())<10?('0'+D):D;
+    DDD = (DDDD=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][dateObject.getDay()]).substring(0,3);
+    th=(D>=10&&D<=20)?'th':((dMod=D%10)==1)?'st':(dMod==2)?'nd':(dMod==3)?'rd':'th';
+    formatString = formatString.replace("#YYYY#",YYYY).replace("#YY#",YY).replace("#MMMM#",MMMM).replace("#MMM#",MMM).replace("#MM#",MM).replace("#M#",M).replace("#DDDD#",DDDD).replace("#DDD#",DDD).replace("#DD#",DD).replace("#D#",D).replace("#th#",th);
+
+    h=(hhh=dateObject.getHours());
+    if (h==0) h=24;
+    if (h>12) h-=12;
+    hh = h<10?('0'+h):h;
+    AMPM=(ampm=hhh<12?'am':'pm').toUpperCase();
+    mm=(m=dateObject.getMinutes())<10?('0'+m):m;
+    ss=(s=dateObject.getSeconds())<10?('0'+s):s;
+    return formatString.replace("#hhh#",hhh).replace("#hh#",hh).replace("#h#",h).replace("#mm#",mm).replace("#m#",m).replace("#ss#",ss).replace("#s#",s).replace("#ampm#",ampm).replace("#AMPM#",AMPM);
+};
+
+var getDuration = function(time) {
+	var duration = "";
+	var cd = 24 * 60 * 60 * 1000;
+    var ch = 60 * 60 * 1000;
+    var d = Math.floor(time / cd);
+    if (d == 1) {
+    	duration += d + " day ";
+    } else if (d > 1) {
+    	duration += d + " days ";
+    }
+    var h = Math.floor( (time - d * cd) / ch);
+    if (h == 1) {
+    	duration += h + " hour ";
+    } else if (h > 1) {
+    	duration += h + " hours ";
+    }
+    var m = Math.floor( (time - d * cd - h * ch) / 60000);
+    if (m == 1) {
+    	duration += m + " minute ";
+    } else if (m > 1) {
+    	duration += m + " minutes ";
+    }
+    var s = Math.round( (time - d * cd - h * ch - m * 60000) / 1000);
+    if (s == 1) {
+    	duration += s + " second ";
+    } else if (s > 1) {
+    	duration += s + " seconds ";
+    }
+    return duration;
+
+};
+
+Date.prototype.getDiff = function(date) {
+	var dateObject = this;
+	var diff = date - dateObject.getTime();
+
+};
