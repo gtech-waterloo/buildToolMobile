e9d94b0dc3790f8da089cc527c3a3c5532d041d8
unknown <james.zhou@CA04316D.global.local>
2015-01-08 17:40:35 -0500

    rename app controller

---
+++ js/mobileApp.js
@@ -0,0 +1,134 @@
+//
+// Here is how to define your module
+// has dependent on mobile-angular-ui
+//
+var app = angular.module('mobildApp', [
+    'ngRoute',
+    'mobile-angular-ui',
+
+    // touch/drag feature: this is from 'mobile-angular-ui.gestures.js'
+    // it is at a very beginning stage, so please be careful if you like to use
+    // in production. This is intended to provide a flexible, integrated and and
+    // easy to use alternative to other 3rd party libs like hammer.js, with the
+    // final pourpose to integrate gestures into default ui interactions like
+    // opening sidebars, turning switches on/off ..
+    'mobile-angular-ui.gestures',
+
+    'buildTool.resources',
+    'projectTool.dateTimeFilter'
+]);
+
+//
+// You can configure ngRoute as always, but to take advantage of SharedState location
+// feature (i.e. close sidebar on backbutton) you should setup 'reloadOnSearch: false'
+// in order to avoid unwanted routing.
+//
+app.config(function($routeProvider) {
+    $routeProvider.when('/',                          {templateUrl: 'home.html',   controller: 'HomeCtrl',     reloadOnSearch: false});
+    $routeProvider.when('/buildTool/projects',        {templateUrl: 'template/buildTool/projects.html', controller: 'ProjectsCtrl', reloadOnSearch: false});
+    $routeProvider.when('/buildTool/project/:name',   {templateUrl: 'template/buildTool/project.html',  controller: 'ProjectCtrl', reloadOnSearch: false});
+});
+
+
+
+//
+// For this trivial demo we have just a unique MainController
+// for everything
+//
+app.controller('MainController', function($rootScope, $scope){
+
+    // User agent displayed in home page
+    $scope.userAgent = navigator.userAgent;
+
+    // Needed for the loading screen
+    $rootScope.$on('$routeChangeStart', function(){
+        $rootScope.loading = true;
+    });
+
+    $rootScope.$on('$routeChangeSuccess', function(){
+        $rootScope.loading = false;
+    });
+});
+
+app.controller('HomeCtrl', function($rootScope, $scope, $controller){
+    $controller('MainController', {$rootScope: $rootScope, $scope: $scope});
+    $rootScope.title = "Home";
+});
+
+app.controller('BuildToolAbsCtrl', function($rootScope, $scope, $controller){
+    $controller('MainController', {$rootScope: $rootScope, $scope: $scope});
+    $rootScope.title = "Build Tool";
+});
+app.controller('ProjectsCtrl', function($rootScope, $scope, $controller, Project){
+    $controller('BuildToolAbsCtrl', {$rootScope: $rootScope, $scope: $scope});
+
+    $scope.projects = Project.query();
+});
+app.controller('ProjectCtrl', function($rootScope, $scope, $http, $routeParams, $controller, Project, Build){
+    $controller('BuildToolAbsCtrl', {$rootScope: $rootScope, $scope: $scope});
+
+    $scope.project = Project.get({name: $routeParams.name}, function(project){
+        $scope.builds = Build.query({projectId: project.id},
+                function(builds) {
+                    checkBuilds(builds);
+        });
+    });
+
+    $scope.build = function() {
+        $http.get("/ProjectTools/app/project/build/" + $scope.project.name).success(
+            function(data){
+                if (data.message == undefined) {
+                    $scope.builds = Build.query({projectId: $scope.project.id},
+                        function(builds) {
+                    });
+                } else {
+                    $scope.launch("notify", "Build notification", data.message);
+                }
+            }
+        );
+    };
+
+    var checkBuildProgress = function() {
+        $scope.builds = Build.query({projectId: $scope.project.id},
+            function(builds) {
+                checkBuilds(builds);
+        });
+    };
+
+    var checkBuilds = function(builds) {
+        builds.forEach(function(build){
+            if (build.buildStatus != 4 && build.buildStatus != 5) {
+//				tid = setTimeout(checkBuildProgress, 2000);
+                return;
+            }
+        });
+    };
+
+    $scope.refresh = function() {
+        checkBuildProgress();
+    };
+
+    $scope.startSchedulerRun = function() {
+        $http.get("/ProjectTools/app/project/schedule/1/" + $scope.project.name).success(
+            function(data){
+                if (data.message == undefined) {
+                    $scope.project.schedulerEnabled = 1;
+                } else {
+                    $scope.launch("notify", "Build notification", data.message);
+                }
+            }
+        );
+    };
+
+    $scope.stopSchedulerRun = function() {
+        $http.get("/ProjectTools/app/project/schedule/0/" + $scope.project.name).success(
+            function(data){
+                if (data.message == undefined) {
+                    $scope.project.schedulerEnabled = 0;
+                } else {
+                    $scope.launch("notify", "Build notification", data.message);
+                }
+            }
+        );
+    };
+});
