e46045002cf3f1b6716e355d68c4d898389435e7
unknown <james.zhou@CA04316D.global.local>
2015-01-08 15:28:00 -0500

    incremental

--- js/demo.js
+++ js/demo.js
@@ -3,16 +3,18 @@
 // has dependent on mobile-angular-ui
 //
 var app = angular.module('MobileAngularUiExamples', [
-  'ngRoute',
-  'mobile-angular-ui',
+    'ngRoute',
+    'mobile-angular-ui',
 
-  // touch/drag feature: this is from 'mobile-angular-ui.gestures.js'
-  // it is at a very beginning stage, so please be careful if you like to use
-  // in production. This is intended to provide a flexible, integrated and and
-  // easy to use alternative to other 3rd party libs like hammer.js, with the
-  // final pourpose to integrate gestures into default ui interactions like
-  // opening sidebars, turning switches on/off ..
-  'mobile-angular-ui.gestures'
+    // touch/drag feature: this is from 'mobile-angular-ui.gestures.js'
+    // it is at a very beginning stage, so please be careful if you like to use
+    // in production. This is intended to provide a flexible, integrated and and
+    // easy to use alternative to other 3rd party libs like hammer.js, with the
+    // final pourpose to integrate gestures into default ui interactions like
+    // opening sidebars, turning switches on/off ..
+    'mobile-angular-ui.gestures',
+
+    'buildTool.resources'
 ]);
 
 //
